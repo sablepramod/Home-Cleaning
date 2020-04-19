1ff7692cbfd91a6ed6d097ccbb9b7b5e86633715
hp <hp@192.168.0.13>
2020-04-19 15:30:05 +0530

    first commit

---
+++ Home-Cleaning/src/main/java/com/pack/homecare/HomeCleaningApplication.java
@@ -0,0 +1,13 @@
+package com.pack.homecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeCleaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeCleaningApplication.class, args);
+	}
+
+}
---
+++ Home-Cleaning/src/test/java/com/pack/homecare/HomeCleaningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.homecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCleaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
